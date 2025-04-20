562e7c4f5b04b90c4529a812927bca2ccf7e8b59
AnasAlmukahal <manstranger29@gmail.com>
2025-04-20 11:19:38 +0300

    hidden keys, added .gitignore

--- grad/firebase.js
+++ grad/firebase.js
@@ -1,17 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
-
-// Your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAVyT8Xo9D65daMqvg4seqmlYlXOGwdvdE",
-  authDomain: "signup-login-2b071.firebaseapp.com",
-  projectId: "signup-login-2b071",
-  storageBucket: "signup-login-2b071.firebasestorage.app",
-  messagingSenderId: "210147322194",
-  appId: "1:210147322194:web:039e01003ec94cf07cb28b",
-  measurementId: "G-M0NPF8P8S1"
-};
+import { firebaseConfig } from "./firebase-config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
