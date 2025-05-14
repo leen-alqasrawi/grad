d025faae13a1c3808eb43567e7283c12aee4a5cc
AnasAlmukahal <manstranger29@gmail.com>
2025-05-14 20:37:34 +0300

    node modules addition

--- grad/firebase-config.js
+++ grad/firebase-config.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-export const firebaseConfig = {
-    "apiKey": "AIzaSyAVyT8Xo9D65daMqvg4seqmlYlXOGwdvdE",
-    "authDomain": "signup-login-2b071.firebaseapp.com",
-    "projectId": "signup-login-2b071",
-    "storageBucket": "signup-login-2b071.firebasestorage.app",
-    "messagingSenderId": "210147322194",
-    "appId": "1:210147322194:web:039e01003ec94cf07cb28b",
-    "measurementId": "G-M0NPF8P8S1"
-=======
+
 export const firebaseConfig={
   apiKey: "AIzaSyAVyT8Xo9D65daMqvg4seqmlYlXOGwdvdE",
   authDomain: "signup-login-2b071.firebaseapp.com",
@@ -17,6 +8,5 @@ export const firebaseConfig={
   messagingSenderId: "210147322194",
   appId: "1:210147322194:web:039e01003ec94cf07cb28b",
   measurementId: "G-M0NPF8P8S1"
->>>>>>> c78fb79577b28ee204c9f963568831aab953067d
   }
   
